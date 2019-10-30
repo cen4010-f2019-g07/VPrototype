2af17c6add30c98600e8612548bcdf8e4388dcf0
AkivaGreen <green.akiva@outlook.com>
2019-10-29 22:30:05 -0400

    Edited Database Connection due to Asynchronous Timing

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
 const app = express();
+var queryString;
+
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'user',
 	password: 'password',
 	database: 'vprototype'
 });
-connection.connect((err) => {
-	if(err) throw err;
-	console.log('Connected!');
-});
+
+function connectDB() {
+		connection.connect((err) => {
+		if(err) throw err;
+		console.log('Connected!');
+	});
+}
+
+function endDBConnect(){
+	connection.end();
+}
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,14 +29,23 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
+	connectDB();
 	res.render('index')
+	endDBConnect();
 });
 
 app.post('/', (req, res) => {
-	console.log(req.body.mysql_query);
+	queryString = req.body.mysql_query;
+	console.log(queryString);
+	connectDB();
+	connection.query(queryString, function(err, rows, fields) {
+		if (err) throw err;
+		for (var i in rows){
+			console.log('Post Titles: ', rows[i].post_title);
+		}
+	});
+	endDBConnect();
 	res.render('index');
 })
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
-
-connection.end();
