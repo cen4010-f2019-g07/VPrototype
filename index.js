a5d0bc27db32202468a266d3bac3b46a67a8af1a
AkivaGreen <green.akiva@outlook.com>
2019-10-29 22:50:07 -0400

    Adjusted Asynchronous database connection and error throwing

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const bodyParser = require('body-parser');
 const app = express();
 var queryString;
 
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'user',
 	password: 'password',
 	database: 'vprototype'
 });
 
-function connectDB() {
-		connection.connect((err) => {
-		if(err) throw err;
-		console.log('Connected!');
-	});
-}
+connection.connect((err) => {
+	if(err) throw err;
+	console.log('Connected!');
+});
 
+/*
 function endDBConnect(){
 	connection.end();
 }
+*/
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,23 +29,33 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-	connectDB();
 	res.render('index');
-	endDBConnect();
 });
 
 app.post('/', (req, res) => {
 	queryString = req.body.mysql_query;
 	console.log(queryString);
-	connectDB();
 	connection.query(queryString, function(err, rows, fields) {
-		if (err) throw err;
+		if (err) {
+			console.log('Bad Input!');
+			//throw err;
+		}
 		for (var i in rows){
 			console.log('Post Titles: ', rows[i].post_title);
 		}
 	});
-	endDBConnect();
 	res.render('index');
 })
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
+
+connection.on('close', function(err){
+	if (err) {
+		//Oops! Unexpected closing of connection, lets reconnect back.
+		connection = mysql.createConnection(connection.config);
+	}
+	else{
+		console.log('Connection closed normally.');
+	}
+
+})
