0ad71159b7df55d15bc3c498102fb156db0b212a
AkivaGreen <green.akiva@outlook.com>
2019-10-29 21:18:22 -0400

    Added bootstrap and Basic form for MySQL

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
-
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'user',
@@ -14,12 +15,18 @@ connection.connect((err) => {
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
 	res.render('index')
 });
+
+app.post('/', (req, res) => {
+	console.log(req.body.mysql_query);
+	res.render('index');
+})
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
 
