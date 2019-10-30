617deb19f18b2c76847c61b1962e0785d429f19e
AkivaGreen <green.akiva@outlook.com>
2019-10-29 20:07:47 -0400

    Added ExpressJS and Basic get request

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+const express = require('express');
+const app = express();
+
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'user',
 	password: 'password',
-	database: 'vprototype_db'
+	database: 'vprototype'
 });
 connection.connect((err) => {
 	if(err) throw err;
 	console.log('Connected!');
 });
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
+
+connection.end();
