fc25d087f199be48ed3ae412aa6727061a2a89e6
AkivaGreen <green.akiva@outlook.com>
2019-10-30 20:31:23 -0400

    resolve issue with submitting query

--- index.js
+++ index.js
@@ -17,19 +17,46 @@ connection.connect((err) => {
 	console.log('Connected!');
 });
 
-/*
-function endDBConnect(){
-	connection.end();
-}
-*/
-
 app.use(express.static('public'));
+app.use(express.static('node_modules'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
-	res.render('pages/index');
+	var issues = [];
+	var events = [];
+	var users = [];
+	var garages = [];
+	connection.query('SELECT * FROM issues', function(err, rows, fields) {
+		if(err) throw err;
+		for (var i in rows){
+			issues[i] = rows[i].post_title;
+		}
+	});
+	connection.query('SELECT * FROM events', function(err, rows, fields) {
+		if(err) throw err;
+		for (var i in rows){
+			events[i] = rows[i].post_title;
+		}
+	});
+	connection.query('SELECT * FROM users', function(err, rows, fields) {
+		if(err) throw err;
+		for (var i in rows){
+			users[i] = rows[i].post_title;
+		}
+	});
+	connection.query('SELECT * FROM garages', function(err, rows, fields) {
+		if(err) throw err;
+		for (var i in rows){
+			garages[i] = rows[i].post_title;
+		}
+	});
+	res.render('pages/index', {
+		issues: issues,
+		events: events,
+		users: users,
+		garages: garages
+	});
 });
 
 app.post('/', (req, res) => {
@@ -38,13 +65,45 @@ app.post('/', (req, res) => {
 	connection.query(queryString, function(err, rows, fields) {
 		if (err) {
 			console.log('Bad Input!');
-			//throw err;
 		}
 		for (var i in rows){
 			console.log('Post Titles: ', rows[i].post_title);
 		}
 	});
-	res.render('pages/index');
+	var issues = [];
+	var events = [];
+	var users = [];
+	var garages = [];
+	connection.query('SELECT * FROM issues', function(err, rows, fields) {
+		if(err) throw err;
+		for (var i in rows){
+			issues[i] = rows[i].post_title;
+		}
+	});
+	connection.query('SELECT * FROM events', function(err, rows, fields) {
+		if(err) throw err;
+		for (var i in rows){
+			events[i] = rows[i].post_title;
+		}
+	});
+	connection.query('SELECT * FROM users', function(err, rows, fields) {
+		if(err) throw err;
+		for (var i in rows){
+			users[i] = rows[i].post_title;
+		}
+	});
+	connection.query('SELECT * FROM garages', function(err, rows, fields) {
+		if(err) throw err;
+		for (var i in rows){
+			garages[i] = rows[i].post_title;
+		}
+	});
+	res.render('pages/index', {
+		issues: issues,
+		events: events,
+		users: users,
+		garages: garages
+	});
 })
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
@@ -57,5 +116,4 @@ connection.on('close', function(err){
 	else{
 		console.log('Connection closed normally.');
 	}
-
-})
+});
